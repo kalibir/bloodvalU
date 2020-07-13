6c918b3bb8f8934d9749e541b729832bd1054751
Edina Lovasz <edina.lovasz@gmail.com>
2020-07-13 17:27:35 +0200

    created reg seeker, code sent and login pages

--- frontend/src/components/Credentials/index.js
+++ frontend/src/components/Credentials/index.js
@@ -1,17 +1,17 @@
 import React from "react";
-//import Login from "./Login";
+import Login from "./Login";
 import {Route} from "react-router-dom";
-//import Verification from "./Verification";
-import RegistrationSeeker from "./RegistrationSeeker";
-//import SignUpSent from "./RegistrationSent";
+import ValidationSeeker from "./ValidationSeeker";
+import Registration from "./Registration";
+import CodeSent from "./CodeSent";
 
 const Credentials = (props) => {
     return (
         <>
-            {/*<Route path="/auth/login" exact component={Login}/>*/}
-            {/*<Route path="/auth/signup" exact component={SignUp}/>*/}
-            <Route path="/auth/registration/seeker" exact component={RegistrationSeeker}/>
-            {/*<Route path="/auth/signup/sent" exact component={SignUpSent}/>*/}
+            <Route path="/auth/login" exact component={Login}/>
+            <Route path="/auth/signup" exact component={Registration}/>
+            <Route path="/auth/signup/validation/seeker" exact component={ValidationSeeker}/>
+            <Route path="/auth/signup/sent" exact component={CodeSent}/>
             {/*<Route path="/auth/signup/validation" exact component={Verification}/>*/}
         </>
     )
