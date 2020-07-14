438524a709c9b2b2b47fc8a7a90221acdc822650
Habib Kadiri <kadirit@hotmail.com>
2020-07-14 16:10:49 +0200

    more migrations

--- backend/apps/donorprofiles/models.py
+++ backend/apps/donorprofiles/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
 # Create your models here.
@@ -8,16 +9,15 @@ User = get_user_model()
 class DonorProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name='donor_profile')
 
-    certificates = models.FileField(upload_to='documents/',
-                                    content_types=['application/pdf'])
+    certificates = models.FileField(null=True, blank=True, validators=[FileExtensionValidator(['pdf'])])
+
     is_valid = models.BooleanField(default=False)
 
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default="name")
 
     website = models.CharField(max_length=150, blank=True, null=True)
 
-    phone = models.CharField(max_length=50)
-
+    phone = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return f'User ID: {self.user.id} Donor Profile'
