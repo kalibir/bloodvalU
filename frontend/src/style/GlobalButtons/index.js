8f2273d4b64915800583cc4945e01acebe399106
Edina Lovasz <edina.lovasz@gmail.com>
2020-07-14 11:38:15 +0200

    working on global styling

--- frontend/src/style/GlobalButtons/index.js
+++ frontend/src/style/GlobalButtons/index.js
@@ -13,38 +13,71 @@ export const DarkBlueButton = styled.button`
     font-weight: normal;
     font-size: 14px;
     line-height: 16px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
     letter-spacing: 0.5px;
     color: #FFFFFF;
+    transition-duration: 0.4s;
 
-  :hover {
-    border: 1px solid #E5E5E5;;
-  }
-  :active {
-    background-color: #e35d5d;
+    :hover {
+        color: #121232;
+        background: #FFF;
+        border: 1px solid #121232;
+    }
+    :active {
+        color: #121232;
+        background: #FFF;
+        border: 1px solid #121232;
+    }
 `;
 
+export const WhiteButton = styled.button`
+    background: #FFFFFF;
+    border: 1px solid #121232;
+    box-sizing: border-box;
+    border-radius: 4px;
+    outline: none;
+    width: ${rem("106px")};
+    height: ${rem("48px")};
+    cursor: pointer;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 14px;
+    line-height: 16px;
+    letter-spacing: 0.5px;
+    color: #121232;
+    transition-duration: 0.4s;
+   
+    :hover {
+        color: #FFF;
+        background: #121232;
+        border: 1px solid #FFF;
+    }
+    :active {
+        color: #FFF;
+        background: #121232;
+        border: 1px solid #FFF;
+    }
+`
+
 export const ChooseRoleButton = styled.button`
-  width: ${rem("352px")};
-  height: ${rem("160px")};
-  background: #FFFFFF;
-  border: 1px solid #D3D4D8;
-  box-sizing: border-box;
-  border-radius: 4px;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 18px;
-  line-height: 24px;
-  text-align: center;
-  color: #121232;
+    width: ${rem("352px")};
+    height: ${rem("160px")};
+    background: #FFFFFF;
+    border: 1px solid #D3D4D8;
+    border-radius: 4px;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 24px;
+    color: #121232;
+    transition-duration: 0.4s;
   
-  :hover {
-    border: 2px solid #E5E5E5;
-    background: #fcfcfc;
-  }
-  :active {
-    border: 2px solid #E5E5E5;
-    background: #fcfcfc;
+    :hover {
+        color: #FFF;
+        border: 2px solid #121232;
+        background: #121232;
+    }
+    :active {
+        color: #FFF;
+        border: 2px solid #121232;
+        background: #121232;
 `;
--- frontend/src/style/GlobalTitles/index.js
+++ frontend/src/style/GlobalTitles/index.js
@@ -1,15 +1,26 @@
 import styled from "styled-components";
 import { rem } from "polished";
 
-export const BlackTitle = styled.h2`
-  font-style: normal;
-  font-weight: normal;
-  font-size: 48px;
-  line-height: 56px;
-  color: #000000;
+export const SmallTitle = styled.h4`
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: ${rem("16px")};
+    color: #232735;
 `;
 
-export const RedTitle = styled(BlackTitle)`
-  color: #E57373;
-  font-weight: bold;
+export const MiddleTitle = styled.h3`
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: ${rem("32px")};
+    color: #232735;
 `;
+
+export const BigTitle = styled.h1`
+    font-style: normal;
+    font-weight: 500;
+    font-size: 32px;
+    line-height: 48px;
+    color: #232735;
+`
