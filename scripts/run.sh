83e35618251e75784c0d52ff6d25f2c5893cb1c2
Habib Kadiri <kadirit@hotmail.com>
2020-07-14 09:37:14 +0200

    fixing scripts file

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
+python manage.py makemigrations
 python manage.py migrate
 rm -rf /frontend/build/* && cp -r /frontend_tmp/build/* /frontend/build
 python manage.py collectstatic --no-input
-gunicorn -w 4 -b 0.0.0.0:8000 project.wsgi:application
+gunicorn -w 4 -b 0.0.0.0:8000 bloodv_project.wsgi:application
