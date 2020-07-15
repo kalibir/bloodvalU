f731fc27e942364039c69132556db659bd252602
António Meireles <edgarmeireles68@gmail.com>
2020-07-15 14:42:47 +0200

    donor request bar

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,13 +1,10 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Template from "../components/Test";
 import Credentials from "../components/Credentials";
 import Navigation from "../components/Navigation";
-import {LandingPage} from "../components/LandingPage";
+import { LandingPage } from "../components/LandingPage";
+import GenericDonorRequestBar from "../components/GenericDonorRequestBar";
 
 const Routes = () => {
   return (
@@ -18,6 +15,7 @@ const Routes = () => {
             <Route path="/templates" component={Template} />
             <Route path="/home" component={LandingPage} />
             <Route path="/auth" component={Credentials} />
+            {/* <Route path="/donorbartest" component={GenericDonorRequestBar} /> */}
             {/*<Route path="/auth" component={Credentials} />*/}
             {/*<Route path="/auth" component={Credentials} />*/}
           </Navigation>
