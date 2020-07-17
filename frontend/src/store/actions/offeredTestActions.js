e87f841d68bbd55bead388ce3f2c5a0bb515c53f
António Meireles <edgarmeireles68@gmail.com>
2020-07-17 17:17:03 +0200

    created seeker profile page

--- frontend/src/store/actions/offeredTestActions.js
+++ frontend/src/store/actions/offeredTestActions.js
@@ -1,10 +1,24 @@
 import Axios from "../../axios";
-import {SET_TESTS} from "../actionTypes";
-
+import { SET_TESTS } from "../actionTypes";
 
 export const setAllTests = (arrayOfTests) => {
-    return{
-        type: SET_TESTS,
-        payload: arrayOfTests
-    }
-}
+  return {
+    type: SET_TESTS,
+    payload: arrayOfTests,
+  };
+};
+
+export const getRequestsOfSeekerAction = () => async (dispatch) => {
+  try {
+    const response = await Axios.get(`seeker/tests/me/`);
+    const { data } = response;
+    console.log("data", data);
+    dispatch(setAllTests(data));
+    console.log("in the action");
+    return response;
+  } catch (error) {
+    console.log("error message", error.response);
+    console.log("error", error);
+    return error;
+  }
+};
