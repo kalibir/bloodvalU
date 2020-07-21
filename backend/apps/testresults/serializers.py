5cc315a705b02c0f7d4b97bf0d4cb58cd8595d1c
Habib Kadiri <kadirit@hotmail.com>
2020-07-21 18:23:14 +0200

    fixing ci cd 2

--- backend/apps/testresults/serializers.py
+++ backend/apps/testresults/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
+from apps.donorprofiles.models import DonorProfile
 from apps.donorprofiles.serializers import DonorProfileSerializer
+from apps.registrations.models import get_or_none
 from apps.testresults.models import TestResult
 
 
@@ -9,4 +11,17 @@ class TestResultSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = TestResult
-        fields = ['donor', 'type_of_test', 'result_code', 'results']
+        fields = ['id', 'donor', 'type_of_test', 'result_code', 'results']
+
+    def validate(self, data):
+        if self.context.get('request').user.is_donor:
+            raise serializers.ValidationError({"detail": "Only Seekers can upload results!"})
+        if not self.context.get('request').user.seeker_profile.is_valid:
+            raise serializers.ValidationError(
+                {"detail": "Only validated Seekers can make upload results, please wait for validation!"})
+        if not get_or_none(DonorProfile, id=data.get('donor_id')):
+            raise serializers.ValidationError({"detail": "This donor does not exist!"})
+        if not get_or_none(DonorProfile, id=data.get('test_id')):
+            raise serializers.ValidationError({"detail": "This offered test does not exist!"})
+
+        return data
--- backend/apps/testresults/views.py
+++ backend/apps/testresults/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from apps.bloodrequests.permissions import IsRequesterOrAdminOrReadOnly
+from apps.donorprofiles.models import DonorProfile
+from apps.offeredtests.models import OfferedTest
+from apps.testresults.serializers import TestResultSerializer
+from apps.users.permissions import ReadOnly
+
+
+class CreateTestResultView(CreateAPIView):
+    serializer_class = TestResultSerializer
+    permission_classes = [IsAuthenticated | ReadOnly]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        target_donor = DonorProfile.objects.get(id=self.kwargs['donor_id'])
+        target_test = OfferedTest.objects.get(id=self.kwargs['test_id'])
+        serializer.save(donor=target_donor, type_of_test=target_test)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
