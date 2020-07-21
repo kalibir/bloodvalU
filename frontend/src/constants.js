8e482742f806770b365ec12f82e99611e667dcef
Habib Kadiri <kadirit@hotmail.com>
2020-07-21 12:04:00 +0200

    fixing ci cd 2

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
-const baseUrl = "http://localhost:8000/backend/api/"; ////
-// const baseUrl = 'https://team-scorpio.propulsion-learn.ch/backend/api/';//
+//const baseUrl = "http://localhost:8000/backend/api/"; ////
+const baseUrl = 'https://blood-value.propulsion-learn.ch/backend/api/';//
 
 export const defaultUserProfileBanner = "https://via.placeholder.com/1200x300";
 
