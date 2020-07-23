67e6ada72c8364cd61bf52c52090675942e71e5a
Habib Kadiri <kadirit@hotmail.com>
2020-07-23 15:07:51 +0200

    fixing ci cd 2

--- backend/apps/donorprofiles/serializers.py
+++ backend/apps/donorprofiles/serializers.py
@@ -5,10 +5,20 @@ from apps.offeredtests.serializers import BoughtTestsOnProfileSerializer
 
 
 class DonorProfileSerializer(serializers.ModelSerializer):
-    bought_tests = BoughtTestsOnProfileSerializer(required=False, many=True)
-
+    no_of_bought_tests = serializers.SerializerMethodField()
     is_donor = serializers.SerializerMethodField()
     email = serializers.SerializerMethodField()
+    no_of_applications = serializers.SerializerMethodField()
+    no_of_test_results = serializers.SerializerMethodField()
+
+    def get_no_of_test_results(self, obj):
+        return obj.test_results.count()
+
+    def get_no_of_applications(self, obj):
+        return obj.applied_to_requests.count()
+
+    def get_no_of_bought_tests(self, obj):
+        return obj.bought_tests.count()
 
     def get_is_donor(self, obj):
         return obj.user.is_donor
@@ -19,7 +29,7 @@ class DonorProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = DonorProfile
         fields = ['id', 'phone', 'is_donor', 'email', 'can_donate', 'can_donate', 'first_name', 'last_name', 'country',
-                  'zip_code', 'street',
+                  'zip_code', 'street', 'no_of_bought_tests',
                   'avatar', 'birthday', 'has_been_selected',
-                  'next_donation', 'total_points', 'blood_group', 'gender', 'age', 'bought_tests',
-                  'applied_to_requests', 'accepted_requests', 'test_results']
+                  'next_donation', 'total_points', 'blood_group', 'gender', 'age',
+                  'no_of_applications', 'no_of_test_results']
