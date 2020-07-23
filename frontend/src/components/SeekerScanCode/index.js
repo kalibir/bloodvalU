6e97335a56ff14d7888631d4aee5bfb5f1bb5100
Habib Kadiri <kadirit@hotmail.com>
2020-07-23 19:52:30 +0200

    fixing ci cd 2

--- frontend/src/components/SeekerScanCode/index.js
+++ frontend/src/components/SeekerScanCode/index.js
@@ -9,6 +9,10 @@ const SuccessText = styled.h1`
   color: #3eb33e;
   font-size: 40px;
 `
+const ErrorText = styled.h1`
+  color: #b33e3e;
+  font-size: 40px;
+`
 
 const SeekerScanCode = (props) => {
     const dispatch = useDispatch()
@@ -17,6 +21,10 @@ const SeekerScanCode = (props) => {
         scan: null,
     });
     const [QRCode, setQRCode] = useState(null)
+    const [QRresponse, setQRResponse] = useState({
+        isGood: null,
+        message: null
+    })
 
     const handleScan = (scannedInfo) => {
         if (data.scan) setQRCode(data.scan)
@@ -30,16 +38,19 @@ const SeekerScanCode = (props) => {
         width: 320,
     }
     const refs = useRef()
-    const handleSubmitQR = (e) => {
+    const handleSubmitQR = async (e) => {
         if (QRCode) {
             const data = {code: QRCode}
-            dispatch(validateQRCode(data))
+            const response = await dispatch(validateQRCode(data))
+            if (response.status < 300) setQRResponse({...QRresponse, message: response.data.detail, isGood: true})
+            else setQRResponse({...QRresponse, message: response.data.detail, isGood: false})
         }
-
     }
 
     return (
         <PageContainer>
+            {QRresponse.message ? QRresponse.isGood ? <SuccessText>{QRresponse.message}</SuccessText> :
+                <ErrorText>{QRresponse.message}</ErrorText> : null}
             {QRCode ? <SuccessText>Code is {QRCode}!</SuccessText> : <p>Scanning, please hold still...</p>}
             <QrReader
                 ref={refs}
