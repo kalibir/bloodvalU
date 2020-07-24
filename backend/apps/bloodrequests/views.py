4c0c91062650d7f4b89f17720073f00d9671f1a0
Edina Lovasz <edina.lovasz@gmail.com>
2020-07-24 07:28:08 +0200

    sending email

--- backend/apps/bloodrequests/views.py
+++ backend/apps/bloodrequests/views.py
@@ -54,9 +54,7 @@ class CreateBloodRequestView(CreateAPIView):
         target_emails = self.get_target_blood_type_emails(serializer.validated_data.get('blood_group'))
         blood_type = serializer.validated_data.get('blood_group')
         seeker_name = request.user.seeker_profile.name
-        donor_name =
         if target_emails:
-            # v Edina
             subject = 'We are looking for a donor'
             message = f'We are looking for a donor'
             to = target_emails
@@ -75,7 +73,7 @@ class CreateBloodRequestView(CreateAPIView):
                                 <a href="https://blood-value.propulsion-learn.ch/dashboard/donor">here.</a></p>
                                 <p>&nbsp;</p>
                                 <p>Best regards,</p>
-                                <p>(seeker_name)</p>
+                                <p>{seeker_name}</p>
                                 <p>&nbsp;</p>
                                 <p><a href="https://blood-value.propulsion-learn.ch">
                                 <span style="color: #262541; font-size: 18px; font-weight: 600;">
@@ -85,16 +83,6 @@ class CreateBloodRequestView(CreateAPIView):
                     </html>"""
             send_mail(subject=subject, message=message, html_message=html_message, from_email=sender, recipient_list=to,
                       fail_silently=False)
-            # ^ Edina
-            # email = EmailMessage()
-            # email.subject = '{seeker_name} is looking for blood donations!'.format(
-            #     seeker_name=request.user.seeker_profile.name)
-            # email.body = '{seeker_name} is looking for donors with a blood type of {request_blood_type}.\nYou your blood type seems to match, be sure to contact them at {seeker_street}, {seeker_zip_code}!'.format(
-            #     seeker_name=request.user.seeker_profile.name,
-            #     request_blood_type=serializer.validated_data.get('blood_group'),
-            #     seeker_street=request.user.seeker_profile.street, seeker_zip_code=request.user.seeker_profile.zip_code)
-            # email.to = target_emails
-            # email.send(fail_silently=False)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.data, status=status.HTTP_201_CREATED)
