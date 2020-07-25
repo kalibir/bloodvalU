fdb50545e1fe21169053cb5ffd0eca9b9714fe62
Habib Kadiri <kadirit@hotmail.com>
2020-07-25 19:54:39 +0200

    fixing ci cd 2

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Template from "../components/Test";
 import Credentials from "../components/Credentials";
 import Navigation from "../components/Navigation";
-import { LandingPage } from "../components/LandingPage";
+import LandingPage from "../components/LandingPage";
 import SeekerProfilePage from "../components/SeekerProfilePage";
 
 import DonorDashboard from "../components/DonorDashboard";
@@ -17,27 +17,27 @@ import AdminPage from "../components/AdminPage";
 import SeekerScanCode from "../components/SeekerScanCode";
 
 const Routes = () => {
-  return (
-    <>
-      <Router>
-        <Switch>
-          <Navigation>
-            <Route path="/dashboard/donor" component={authComponent(DonorDashboard)} />
-            <Route path="/dashboard/seeker" component={authComponent(SeekerDashboard)} />
-            <Route path="/templates" component={Template} />
-            <Route exact path="/" component={authComponent(LandingPage)} />
-            <Route path="/auth" component={Credentials} />
-            <Route path="/editseeker" component={authComponent(SeekerEditProfile)} />
-            <Route path="/editdonor" component={authComponent(DonorEditProfile)} />
-            <Route path="/seekerprofilepage" component={authComponent(SeekerProfilePage)} />
-            <Route path="/map" component={GeoMap} />
-            <Route path="/scan" component={SeekerScanCode} />
-            <Route path="/admin_page" component={AdminPage} />
-          </Navigation>
-        </Switch>
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <Router>
+                <Switch>
+                    <Navigation>
+                        <Route path="/dashboard/donor" component={authComponent(DonorDashboard)}/>
+                        <Route path="/dashboard/seeker" component={authComponent(SeekerDashboard)}/>
+                        <Route path="/templates" component={Template}/>
+                        <Route exact path="/" component={authComponent(LandingPage)}/>
+                        <Route path="/auth" component={Credentials}/>
+                        <Route path="/editseeker" component={authComponent(SeekerEditProfile)}/>
+                        <Route path="/editdonor" component={authComponent(DonorEditProfile)}/>
+                        <Route path="/seekerprofilepage" component={authComponent(SeekerProfilePage)}/>
+                        <Route path="/map" component={GeoMap}/>
+                        <Route path="/scan" component={SeekerScanCode}/>
+                        <Route path="/admin_page" component={AdminPage}/>
+                    </Navigation>
+                </Switch>
+            </Router>
+        </>
+    );
 };
 
 export default Routes;
