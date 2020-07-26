b24265ef73a8117eb6559159c81b3177d9623cad
Habib Kadiri <kadirit@hotmail.com>
2020-07-26 13:23:01 +0200

    fixing git ignore

--- frontend/src/components/SeekerStatistics/index.js
+++ frontend/src/components/SeekerStatistics/index.js
@@ -1,18 +1,58 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {PageContainer} from "../../style/GlobalWrappers";
 import {connect} from "react-redux";
 import {getMyStatisticsAction} from "../../store/actions/bloodRequestActions";
+import {Doughnut} from "react-chartjs-2";
 
+const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getGenderData = (stats) => {
+    return [stats.filter(el => el.gender === "M").length, stats.filter(el => el.gender === "F").length, stats.filter(el => el.gender === "O").length]
+}
 
 const SeekerStatistics = ({userProfileReducer: {statistics}, dispatch}) => {
+    console.log("statistics", statistics);
 
-    useEffect(() => {
-        dispatch(getMyStatisticsAction())
-    },[dispatch])
+    const [state, setState] = useState({
+        labels: [
+            'Male',
+            'Female',
+            'Other'
+        ],
+        datasets: [{
+            data: [0, 0, 0],
+            backgroundColor: [
+                '#36A2EB',
+                '#FFCE56',
+                '#CCC',
+            ],
+            hoverBackgroundColor: [
+                '#36A2EB',
+                '#FFCE56',
+                '#FF6384',
+            ]
+        }]
+    });
+
+
+    useEffect(async () => {
+        const response = await dispatch(getMyStatisticsAction())
+        if (response.status < 300) setState({...state, datasets: {data: getGenderData(response.data)}})
+        // setInterval(() => {
+        //     setState(getState());
+        // }, 2000)
+    }, [dispatch])
 
 
     return (
-        <PageContainer>STATS</PageContainer>
+        <PageContainer>
+            <div>
+                <h2>Gender Demographic</h2>
+                {state ? <Doughnut data={state}/> : null}
+            </div>
+        </PageContainer>
     )
 }
 
