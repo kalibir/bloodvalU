78441a16d25ee5545448cb480c64029b6e14459b
Habib Kadiri <kadirit@hotmail.com>
2020-07-26 13:51:00 +0200

    fixing git ignore

---
+++ frontend/src/components/SeekerStatistics/GenderGraph/index.js
@@ -0,0 +1,51 @@
+import React, {useEffect, useState} from "react";
+import {Pie} from "react-chartjs-2";
+
+const initialState = {
+    labels: [
+        'Male',
+        'Female',
+        'Other'
+    ],
+    datasets: [{
+        data: [0, 0, 0],
+        backgroundColor: [
+            '#36A2EB',
+            '#FFCE56',
+            '#CCC',
+        ],
+        hoverBackgroundColor: [
+            '#36A2EB',
+            '#FFCE56',
+            '#FF6384',
+        ]
+    }]
+}
+
+const getGenderData = (stats) => {
+    return [stats.filter(el => el.gender === "M").length, stats.filter(el => el.gender === "F").length, stats.filter(el => el.gender === "O").length]
+}
+
+
+const GenderGraph = ({statistics}) => {
+
+    const [state, setState] = useState(initialState);
+
+    useEffect(() => {
+        if (statistics) {
+            console.log("we got stats!")
+            const newState = {...state}
+            newState.datasets[0].data = getGenderData(statistics)
+            setState(newState)
+        }
+    }, [statistics])
+
+
+    return (
+        <div>
+            <h2>Gender Demographic</h2>
+            {state ? <Pie data={state}/> : null}
+        </div>)
+}
+
+export default GenderGraph
--- frontend/src/components/SeekerStatistics/index.js
+++ frontend/src/components/SeekerStatistics/index.js
@@ -1,73 +1,30 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import {PageContainer} from "../../style/GlobalWrappers";
 import {connect} from "react-redux";
 import {getMyStatisticsAction} from "../../store/actions/bloodRequestActions";
-import {Doughnut} from "react-chartjs-2";
+import GenderGraph from "./GenderGraph";
 
-const getRandomInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 // setInterval(() => {
 //     setState(getState());
 // }, 2000)
 
-const initialState = {
-    labels: [
-        'Male',
-        'Female',
-        'Other'
-    ],
-    datasets: [{
-        data: [0, 0, 0],
-        backgroundColor: [
-            '#36A2EB',
-            '#FFCE56',
-            '#CCC',
-        ],
-        hoverBackgroundColor: [
-            '#36A2EB',
-            '#FFCE56',
-            '#FF6384',
-        ]
-    }]
-}
-
-const getGenderData = (stats) => {
-    return [stats.filter(el => el.gender === "M").length, stats.filter(el => el.gender === "F").length, stats.filter(el => el.gender === "O").length]
-}
 
 const SeekerStatistics = ({userProfileReducer: {statistics}, dispatch}) => {
-
-
-    const [state, setState] = useState(initialState);
 
     useEffect(() => {
         dispatch(getMyStatisticsAction())
     }, [dispatch])
 
-    useEffect(() => {
-        if (statistics) {
-            console.log("we got stats!")
-            const newState = {...state}
-            newState.datasets[0].data = getGenderData(statistics)
-            setState(newState)
-        }
-    }, [statistics])
-
 
     return (
         <PageContainer>
-            <div>
-                <h2>Gender Demographic</h2>
-                {state ? <Doughnut data={state}/> : null}
-            </div>
+            <GenderGraph statistics={statistics}/>
         </PageContainer>
     )
 }
 
 const mapStateToProps = (state) => {
-    console.log("state", state);
     return {
         userProfileReducer: state.userProfileReducer,
     };
