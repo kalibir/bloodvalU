691dedc114f3e304acc9a8f3f630754a3727fb53
Gőz Attila <goz.attila.1977@gmail.com>
2020-07-27 13:01:03 +0200

    Animation adding

--- frontend/src/components/Credentials/CreateBaseUser/index.js
+++ frontend/src/components/Credentials/CreateBaseUser/index.js
@@ -124,7 +124,7 @@ const CreateBaseUser = ({
               </Error>
               <BigInput
                 title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
-                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
                 type="password"
                 placeholder="password"
                 onChange={(e) => onChangeHandler(e, "password")}
