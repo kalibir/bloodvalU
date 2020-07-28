94f6ccd2cba04e467f537ba39474379a4b783405
Habib Kadiri <kadirit@hotmail.com>
2020-07-28 14:42:03 +0200

    added age demo chart

--- frontend/src/components/DonorDashboard/index.js
+++ frontend/src/components/DonorDashboard/index.js
@@ -329,7 +329,6 @@ const DonorDashboard = ({
 };
 
 const mapStateToProps = (state) => {
-    console.log(state);
     return {
         userProfileReducer: state.userProfileReducer,
         authReducer: state.authReducer,
