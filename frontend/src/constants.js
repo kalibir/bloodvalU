579479ccd3fffea1f8375b727f3dafe526a900be
Habib Kadiri <kadirit@hotmail.com>
2020-07-30 21:23:04 +0200

    added age demo chart

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-// const baseUrl = "http://localhost:8000/backend/api/"; ////
+// const baseUrl = "http://localhost:8000/backend/api/";
 const baseUrl = 'https://blood-value.propulsion-learn.ch/backend/api/';//
 
 export const defaultUserProfileBanner = "https://via.placeholder.com/1200x300";
